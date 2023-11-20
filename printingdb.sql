57e7c3d8fb9b84c1d5851ac0293751edcef7ea57
dascy <kaitoukid1305@gmail.com>
2023-11-20 19:15:54 +0700

    db config

--- printingdb.sql
+++ printingdb.sql
@@ -25,12 +25,12 @@ DROP TABLE IF EXISTS `printer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `printer` (
-  `printerID` int NOT NULL,
+  `printerID` int NOT NULL AUTO_INCREMENT,
   `model` varchar(45) NOT NULL,
   `location` varchar(45) DEFAULT 'H1 lobby',
   `status` varchar(45) DEFAULT 'active',
   PRIMARY KEY (`printerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `printer` (
 
 LOCK TABLES `printer` WRITE;
 /*!40000 ALTER TABLE `printer` DISABLE KEYS */;
+INSERT INTO `printer` VALUES (1,'P1 - Lầu 6','CS2 - Lầu 6','Đang hoạt động'),(2,'P1 - Lầu 5','CS2 - Lầu 5','Đang hoạt động'),(3,'P1 - Lầu 4','CS2 - Lầu 4','Đang hoạt động'),(4,'P1 - Lầu 3','CS2 - Lầu 3','Đang hoạt động'),(5,'P1 - Lầu 2','CS2 - Lầu 2','Ngưng hoạt động');
 /*!40000 ALTER TABLE `printer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-20 19:06:40
+-- Dump completed on 2023-11-20 19:15:07
