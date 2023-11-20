01e5dd626981487dc77f358e00112d2e00ce836f
dascy <kaitoukid1305@gmail.com>
2023-11-21 00:54:45 +0700

    printer

--- backend/routes/printerRouter.js
+++ backend/routes/printerRouter.js
@@ -3,9 +3,10 @@ import {
   getAllPrinter,
   addPrinter,
   deletePrinter,
+  updatePrinter,
 } from "../controllers/printerController.js";
 
 const router = Router();
 router.route("/").get(getAllPrinter).post(addPrinter);
-router.route("/:printerID").delete(deletePrinter);
+router.route("/:printerID").delete(deletePrinter).put(updatePrinter);
 export const printerRouter = router;
