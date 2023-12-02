2ab439b7eeabee29ddfe122a58b47cd861353972
dangnguyennhat <129052433+dangnguyennhat@users.noreply.github.com>
2023-12-02 19:36:19 +0700

    general setting

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,14 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 import { printerRouter } from "./routes/printerRouter.js";
+import {gsRouter} from "./routes/gsRouter.js";
 const app = express();
 
 app.use(json());
 app.use(cors());
 
 app.use("/printers", printerRouter);
+app.use("/gs", gsRouter);
 
 app.listen(PORT, () => {
   console.log(`App is running in PORT ${PORT}`);
