360be4b6ec64cc3ea3327a07d8a0477daa63b957
HoangPham1208 <hoang.pham1208@hcmut.edu.vn>
2023-12-04 00:33:19 +0700

    update general setting data

--- frontend/src/SPSO-Func/genaralSetting.js
+++ frontend/src/SPSO-Func/genaralSetting.js
@@ -80,7 +80,8 @@ const GenaralSetting = () => {
               <div class="w-2/3  ">
               <input
                 type="text"
-                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-sm focus:outline-none"
+                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-xl focus:outline-none"
+                value="500"
               />
               </div>
             </div>   
@@ -91,7 +92,9 @@ const GenaralSetting = () => {
                 <div class="w-2/3  ">
               <input
                 type="text"
-                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-sm focus:outline-none "                
+                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-xl focus:outline-none "           
+                value="10/20/2023 10:00 AM"     
+                
               />
               </div>
             </div>   
@@ -99,10 +102,11 @@ const GenaralSetting = () => {
                 <p class="text-xl text-[#114A65] w-64 font-semibold">
                   Kích thước tệp tối đa
                 </p>
-              <div class="relative  ">
+              <div class="relative">
               <input
                 type="text"
-                class="border border-gray-500 bg-white h-10 w-64 px-4 pr-4 rounded-xl text-sm focus:outline-none"
+                class="border border-gray-500 bg-white h-10 w-64 px-4 pr-4 rounded-xl text-xl focus:outline-none"
+                value="500"
               />
               </div>
               <p class="text-xl text-blackw-64 font-semibold ml-8">KB</p>
@@ -113,27 +117,27 @@ const GenaralSetting = () => {
                 </p>
               <div class="grid grid-cols-3 grid-rows-2 ">
                 <div class="flex col-span-1 row-span-1 row-start-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/>
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">PDF</label>
                 </div>
                 <div class="flex col-span-1 row-span-1 row-start-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Image</label>
                 </div>  
                 <div class="flex col-span-1 row-span-1 row-start-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Excel</label>
                 </div> 
                 <div class="flex col-span-1 row-span-1 items-center row-start-2 mt-3 ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Word</label>
                 </div> 
                 <div class="flex col-span-1 row-span-1 items-center row-start-2 mt-3">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">HTML</label>
                 </div> 
                 <div class="flex col-span-1 row-span-1 items-center row-start-2 mt-3  w-40">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Powerpoint</label>
                 </div> 
               </div>
@@ -145,7 +149,8 @@ const GenaralSetting = () => {
               <div class="w-2/3  ">
               <input
                 type="text"
-                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-sm focus:outline-none"
+                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-xl focus:outline-none"
+                value = "500"
               />
               </div>
             </div>           
@@ -153,23 +158,27 @@ const GenaralSetting = () => {
                 <p class="text-xl text-[#114A65] w-64 font-semibold">
                   Khổ giấy cho phép
                 </p>
-              <div class="grid grid-cols-4 ">
+              <div class="grid grid-cols-3 ">
                 <div class="flex col-span-1  items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
-                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A5</label>
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
+                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A4</label>
                 </div>
                 <div class="flex col-span-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
-                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A4</label>
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
+                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A3</label>
                 </div>  
                 <div class="flex col-span-1  items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
-                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A3</label>
-                </div> 
-                <div class="flex col-span-1  items-center w-40">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A2</label>
                 </div> 
+                <div class="flex col-span-1  items-center w-40 mt-4">
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
+                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">A1</label>
+                </div> 
+                <div class="flex col-span-1  items-center w-40 mt-4">
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
+                  <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">B1</label>
+                </div>
               </div>
             </div>  
             <div class="flex items-center">
@@ -178,11 +187,11 @@ const GenaralSetting = () => {
                 </p>
               <div class="grid grid-cols-4 ">
                 <div class="flex col-span-1  items-center w-40">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold ">Đen</label>
                 </div>
                 <div class="flex col-span-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Màu</label>
                 </div>  
               </div>
@@ -193,11 +202,11 @@ const GenaralSetting = () => {
                 </p>
               <div class="grid grid-cols-4 ">
                 <div class="flex col-span-1  items-center w-40  ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold ">Một mặt</label>
                 </div>
                 <div class="flex col-span-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Hai mặt</label>
                 </div>  
               </div>
@@ -208,19 +217,19 @@ const GenaralSetting = () => {
                 </p>
               <div class="grid grid-cols-4 ">
                 <div class="flex col-span-1  items-center w-40">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold ">BKPay</label>
                 </div>
                 <div class="flex col-span-1 items-center ">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Tiền mặt</label>
                 </div>  
                 <div class="flex col-span-1  items-center">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold ">Thẻ tín dụng</label>
                 </div>
                 <div class="flex col-span-1 items-center">
-                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded"/> 
+                  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-black rounded" checked="true"/> 
                   <label for="myCheckbox" class="ml-2 text-xl text-black font-semibold">Momo</label>
                 </div>  
               </div>
@@ -232,7 +241,8 @@ const GenaralSetting = () => {
               <div class="w-2/3  ">
               <input
                 type="text"
-                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-sm focus:outline-none"
+                class="border border-gray-500 bg-white h-10 w-2/3 px-4 pr-4 rounded-xl text-xl focus:outline-none"
+                value="99999"
               />
               </div>
             </div>   
